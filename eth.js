20349c10340540c90dc4bd288cd52696d6e9ce4e
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-05-05 10:13:01 +0700

    adjust eth

--- eth.js
+++ eth.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-param-reassign */
+/* eslint-disable radix */
 /**
  * @author JeffVader
  */
@@ -7,7 +8,13 @@ const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
 
+const BATCH_SIZE = 40000;
 const INFURA_KEY = '2bf416022d924fdcabe3b279f3ac9b90';
+const ETHERSCAN_API_KEY = '2S8HBB49TRGD3APYD6U2FNTUYYUG6HP6BF';
+
+const urls = [
+  `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+];
 
 function writeToFileSync(filepath, args) {
   const flag = 'w';
@@ -16,9 +23,8 @@ function writeToFileSync(filepath, args) {
   fs.closeSync(fd);
 }
 
-async function getRPCResponse(method, params) {
-  const url = `https://mainnet.infura.io/v3/${INFURA_KEY}`;
-  params = params || [];
+async function getETHRPCResponse(method, params = []) {
+  const url = urls[Math.floor(Math.random() * urls.length)];
   const data = {
     jsonrpc: '2.0',
     method,
@@ -29,26 +35,82 @@ async function getRPCResponse(method, params) {
     'Content-Type': 'application/json',
   };
   const response = await axios.post(url, data, headers);
+  // console.log(response.data);
+  if (!response.data.result) {
+    console.log(response.data);
+  }
+  if (response.data.result.length >= 1000) {
+    console.log('WARNING: Max results received - lower the BATCH_SIZE');
+  }
   return response.data.result;
 }
 
-async function getContractTransfers(contract, fromBlock, toBlock) {
+async function getRPCResponse(params = []) {
+  const url = 'https://api.etherscan.com/api';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const response = await axios.get(url, { params }, headers);
+  if (!response.data.result) {
+    console.log(response.data);
+  }
+  return response.data.result;
+}
+
+async function getContractTransferBlock(contract, fromBlock, toBlock) {
   const transferHash = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
-  const params = [{
+  const params = {
     address: contract,
     fromBlock,
     toBlock,
-    topics: [transferHash],
-  }];
-  // Warning: eth_getLogs will fail if there are more than 10,000 records.
-  // - at that point we will need to move to a paging approach.
-  const logs = await getRPCResponse('eth_getLogs', params);
-  logs.forEach((log) => {
-    // eslint-disable-next-line radix
-    log.amount = Number.parseInt(log.data);
-    log.from = log.topics[1].substring(0, 2) + log.topics[1].substring(26);
-    log.to = log.topics[2].substring(0, 2) + log.topics[2].substring(26);
-  });
+    topic0: transferHash,
+    module: 'logs',
+    action: 'getLogs',
+    apikey: ETHERSCAN_API_KEY,
+  };
+  const validResponse = false;
+  while (!validResponse) {
+    // console.log(`From: ${fromBlock} - to: ${toBlock}`);
+    // eslint-disable-next-line no-await-in-loop
+    const logs = await getRPCResponse(params);
+    if (logs) {
+      logs.forEach((log) => {
+        log.amount = Number.parseInt(log.data);
+        log.from = log.topics[1].substring(0, 2) + log.topics[1].substring(26);
+        log.to = log.topics[2].substring(0, 2) + log.topics[2].substring(26);
+      });
+      return logs;
+    }
+  }
+  return [];
+}
+
+async function getContractTransfers(contract, fromBlock, toBlock, batchSize = BATCH_SIZE) {
+  const fromBlockNum = Number.parseInt(fromBlock);
+  const toBlockNum = Number.parseInt(toBlock);
+  const logs = [];
+  let blockProcessStart = fromBlockNum;
+  let blockProcessEnd = (blockProcessStart + batchSize) > toBlockNum ? toBlockNum : (blockProcessStart + batchSize);
+  while (blockProcessStart <= toBlockNum) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const blockLogs = await getContractTransferBlock(contract, blockProcessStart, blockProcessEnd);
+      if (blockLogs.length >= 1000) {
+        blockProcessEnd = Math.ceil(blockProcessEnd - ((blockProcessEnd - blockProcessStart) / 2));
+      } else {
+        logs.push(...blockLogs);
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`Fetching contract ETH TXS from block ${blockProcessStart} to block ${blockProcessEnd}. Remaining Blocks: ${Number(toBlockNum - blockProcessEnd)}`);
+        blockProcessStart = blockProcessEnd + 1;
+        blockProcessEnd = (blockProcessStart + batchSize) > toBlockNum ? toBlockNum : (blockProcessStart + batchSize);
+      }
+    } catch (error) {
+      console.log('ERRORED');
+      console.log(error);
+    }
+  }
+  process.stdout.write('\n');
   return logs;
 }
 
@@ -76,9 +138,14 @@ function getBalanceList(transfers) {
 }
 
 async function start(contract) {
-  const blockNum = await getRPCResponse('eth_blockNumber');
-  const transfers = await getContractTransfers(contract, '0x0', blockNum);
-  const balances = await getBalanceList(transfers);
+  let balances = [];
+  try {
+    const blockNum = await getETHRPCResponse('eth_blockNumber');
+    const transfers = await getContractTransfers(contract, 12643111, blockNum); // 12643111, contract created on 12643112
+    balances = await getBalanceList(transfers);
+  } catch (error) {
+    console.log();
+  }
 
   let total = 0;
   balances.forEach((balance) => {
@@ -87,7 +154,7 @@ async function start(contract) {
 
   const homeDirPath = path.join(__dirname, './export/');
   const filepath = `${homeDirPath}ethlist.json`;
-  console.log(`Done Eth - ${balances.length} records found. Exported at ${filepath}. Total Flux-ETH: ${total.toLocaleString()} (${total}).`);
+  console.log(`Done ETH - ${balances.length} records found. Exported at ${filepath}. Total Flux-ETH: ${total.toLocaleString()} (${total}).`);
   writeToFileSync(filepath, JSON.stringify(balances, null, 2));
 }
 
